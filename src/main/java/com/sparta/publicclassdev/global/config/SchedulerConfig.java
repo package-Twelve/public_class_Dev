97d6e3339295baaef38c70fd4c1b71e9992dcd9a
marlboro09 <taebong8989@gmail.com>
2024-07-31 17:49:26 +0900

    ✨ update : 우승자 조회 페이지 컨트롤러 작성 및 스케줄 실행 시 로그파일 생성되게

--- src/main/java/com/sparta/publicclassdev/global/config/SchedulerConfig.java
+++ src/main/java/com/sparta/publicclassdev/global/config/SchedulerConfig.java
@@ -2,7 +2,9 @@ package com.sparta.publicclassdev.global.config;
 
 import com.sparta.publicclassdev.domain.teams.service.TeamsService;
 import com.sparta.publicclassdev.domain.winners.service.WinnersService;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,6 +12,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 @Configuration
 @EnableScheduling
 @RequiredArgsConstructor
+@Slf4j
 public class SchedulerConfig {
 
     private final TeamsService teamsService;
@@ -17,11 +20,13 @@ public class SchedulerConfig {
     
     @Scheduled(cron = "0 0 0 * * ?")
     public void dailyWinners() {
+        log.info("Executing dailyWinners at {}", LocalDateTime.now());
         winnersService.dailyWinners();
     }
 
     @Scheduled(cron = "0 0 0 * * ?")
     public void deleteTeamsMidnight() {
+        log.info("Executing deleteTeamsMidnight at {}", LocalDateTime.now());
         teamsService.deleteAllTeams();
     }
 }
