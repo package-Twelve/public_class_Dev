bb8c85f46aeae094f8cf24f5fa3bab9ba3d60747
marlboro09 <taebong8989@gmail.com>
2024-08-02 13:22:31 +0900

    ♻️ refactor : 프론트에서 유저권한을 받아오기 위해 response에 getRole을 추가했습니다

--- src/main/java/com/sparta/publicclassdev/domain/users/dto/ProfileResponseDto.java
+++ src/main/java/com/sparta/publicclassdev/domain/users/dto/ProfileResponseDto.java
@@ -1,6 +1,7 @@
 package com.sparta.publicclassdev.domain.users.dto;
 
 import com.sparta.publicclassdev.domain.communities.dto.CommunitiesResponseDto;
+import com.sparta.publicclassdev.domain.users.entity.RoleEnum;
 import com.sparta.publicclassdev.domain.users.entity.Users;
 import java.util.List;
 import lombok.Getter;
@@ -10,12 +11,14 @@ public class ProfileResponseDto {
     private String name;
     private String email;
     private String intro;
+    private RoleEnum role;
     private List<CommunitiesResponseDto> recentCommunities;
 
     public ProfileResponseDto(Users user, List<CommunitiesResponseDto> recentCommunities) {
         this.name = user.getName();
         this.email = user.getEmail();
         this.intro = user.getIntro();
+        this.role = user.getRole();
         this.recentCommunities = recentCommunities;
     }
 
@@ -23,5 +26,6 @@ public class ProfileResponseDto {
         this.name = user.getName();
         this.email = user.getEmail();
         this.intro = user.getIntro();
+        this.role = user.getRole();
     }
 }
