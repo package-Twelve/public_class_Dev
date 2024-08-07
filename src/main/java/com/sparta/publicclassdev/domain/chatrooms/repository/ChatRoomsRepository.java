85583421ba94b7db8e3de828e116d8648ea78f64
marlboro09 <taebong8989@gmail.com>
2024-08-07 11:19:47 +0900

    ♻️ refactor: winner도 request와 response로 dto를 나누어 재작업

--- src/main/java/com/sparta/publicclassdev/domain/chatrooms/repository/ChatRoomsRepository.java
+++ src/main/java/com/sparta/publicclassdev/domain/chatrooms/repository/ChatRoomsRepository.java
@@ -1,9 +1,13 @@
 package com.sparta.publicclassdev.domain.chatrooms.repository;
 
 import com.sparta.publicclassdev.domain.chatrooms.entity.ChatRooms;
+import com.sparta.publicclassdev.domain.teams.entity.Teams;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomsRepository extends JpaRepository<ChatRooms, Long> {
     
     void deleteAllByTeamsId(Long teamsId);
+    
+    List<ChatRooms> findByTeams(Teams teams);
 }
