44f7d9de6acac8c836ae073854fc2979fb72756d
이세비 <lsy205105@naver.com>
2024-08-08 10:14:11 +0900

    🩹 fix : Community Content 글자 제한 해제

--- src/main/java/com/sparta/publicclassdev/domain/communities/dto/CommunitiesRequestDto.java
+++ src/main/java/com/sparta/publicclassdev/domain/communities/dto/CommunitiesRequestDto.java
@@ -1,18 +1,27 @@
 package com.sparta.publicclassdev.domain.communities.dto;
 
 import com.sparta.publicclassdev.domain.communities.entity.Communities.Category;
+import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class CommunitiesRequestDto {
     @NotBlank(message = "제목을 작성해주세요")
     String title;
-    @Size(min = 5, max = 100, message = "5글자 이상 100글자 이하로 작성해주세요")
+    @Column(columnDefinition = "TEXT")
     @NotBlank(message = "내용을 작성해주세요")
     String content;
     @NotNull(message = "카테고리를 지정해주세요")
     Category category;
+
+    @Builder
+    public CommunitiesRequestDto(String title, String content, Category category) {
+        this.title = title;
+        this.content = content;
+        this.category = category;
+    }
 }
