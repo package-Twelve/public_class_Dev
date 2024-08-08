592d420810e58b155fc9f8d5852291c70ae05e7f
Hyeonjin Kim <andrew75313@gmail.com>
2024-08-08 14:10:14 +0900

    ♻️ refactor: 도메인을 통한 접근이 가능하도록 변경

--- src/main/java/com/sparta/publicclassdev/global/config/WebMvcConfig.java
+++ src/main/java/com/sparta/publicclassdev/global/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("http://localhost:3000/")
+            .allowedOriginPatterns("http://publicclassdev.com")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
             .allowCredentials(true);
     }
