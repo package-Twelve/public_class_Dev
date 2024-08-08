2d6e1d2ff13add25254b2a9d82d8d77b34da03ed
marlboro09 <taebong8989@gmail.com>
2024-08-08 19:56:15 +0900

    ♻️ refactor : 코드입력이 빈 칸 입력불가 및 파이썬 실행 수정

--- src/main/java/com/sparta/publicclassdev/domain/coderuns/dto/CodeRunsRequestDto.java
+++ src/main/java/com/sparta/publicclassdev/domain/coderuns/dto/CodeRunsRequestDto.java
@@ -1,11 +1,14 @@
 package com.sparta.publicclassdev.domain.coderuns.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class CodeRunsRequestDto {
+    @NotBlank
     private String language;
+    @NotBlank
     private String code;
 }
