48be649067bd5796a06f2c01b8c502d0de504ce4
marlboro09 <taebong8989@gmail.com>
2024-08-08 20:03:42 +0900

    ♻️ refactor : 코드입력이 빈 칸 입력불가 및 파이썬 실행 수정

--- src/main/java/com/sparta/publicclassdev/domain/coderuns/runner/PythonCodeRunner.java
+++ src/main/java/com/sparta/publicclassdev/domain/coderuns/runner/PythonCodeRunner.java
@@ -14,7 +14,7 @@ public class PythonCodeRunner implements CodeRunner {
     @Override
     public String runCode(String code) {
         validateCode(code);
-        return runScript("python", ".py", code);
+        return runScript("python3", ".py", code);
     }
     
     private void validateCode(String code) {
