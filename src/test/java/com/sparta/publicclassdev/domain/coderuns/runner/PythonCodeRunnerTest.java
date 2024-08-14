b02900930823ef7a9c4d7beac545a9cfb7ae8891
marlboro09 <taebong8989@gmail.com>
2024-08-14 20:45:31 +0900

    🍻 test : PythonCodeRunner 테스트 작성

---
+++ src/test/java/com/sparta/publicclassdev/domain/coderuns/runner/PythonCodeRunnerTest.java
@@ -0,0 +1,35 @@
+package com.sparta.publicclassdev.domain.coderuns.runner;
+
+import com.sparta.publicclassdev.global.exception.CustomException;
+import com.sparta.publicclassdev.global.exception.ErrorCode;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PythonCodeRunnerTest {
+    
+    private CodeRunner codeRunner;
+    
+    @BeforeEach
+    void setUp() {
+        codeRunner = new PythonCodeRunner();
+    }
+    
+    @Test
+    @DisplayName("유효한 Python 코드 테스트")
+    void runValidPythonCode() {
+        String code = "print('Python')";
+        String result = codeRunner.runCode(code);
+        assertTrue(result.contains("Python"));
+    }
+    
+    @Test
+    @DisplayName("허용되지 않는 작업이 포함된 코드 테스트")
+    void runPythonCodeWithDisallowedOperations() {
+        String code = "import os; print('Hello, World!')";
+        CustomException thrown = assertThrows(CustomException.class, () -> codeRunner.runCode(code));
+        assertEquals(ErrorCode.INVALID_CODE, thrown.getErrorCode(), "INVALID_CODE 에러 발생");
+    }
+}
