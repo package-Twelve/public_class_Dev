a23d64fbeaef825587c1d1c9216594af98046214
marlboro09 <taebong8989@gmail.com>
2024-08-19 19:58:24 +0900

    🍻 test : ChatRoomsRepository 테스트 코드 작성

---
+++ src/test/java/com/sparta/publicclassdev/domain/chatrooms/repository/ChatRoomsRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.sparta.publicclassdev.domain.chatrooms.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChatRoomsRepositoryTest {
+    
+    @Test
+    void deleteAllByTeamsId() {
+    }
+    
+    @Test
+    void findByTeams() {
+    }
+}
